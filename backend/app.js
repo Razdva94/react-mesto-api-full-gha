371d08c4ee0cbf7955cba13aceabf45cd1913a99
Razdva94 <115868609+Razdva94@users.noreply.github.com>
2023-07-05 18:35:11 +0300

    cors fix

--- backend/app.js
+++ backend/app.js
@@ -33,19 +33,21 @@ mongoose
   });
 app.use(requestLogger);
 app.options('*', cors());
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'http://razdva.nomoreparties.sbs');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.sendStatus(200);
-});
-app.use(
-  cors({
-    origin: 'http://razdva.nomoreparties.sbs',
-    credentials: true,
-  }),
-);
+app.use(cors());
+// app.options('*', cors());
+// app.options('*', (req, res) => {
+//   res.header('Access-Control-Allow-Origin', 'http://razdva.nomoreparties.sbs');
+//   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   res.header('Access-Control-Allow-Credentials', true);
+//   res.sendStatus(200);
+// });
+// app.use(
+//   cors({
+//     origin: 'http://razdva.nomoreparties.sbs',
+//     credentials: true,
+//   }),
+// );
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');

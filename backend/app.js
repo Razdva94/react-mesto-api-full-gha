3cb9b8e8cb6b6365851ced92893963ea18d9cef1
Razdva94 <115868609+Razdva94@users.noreply.github.com>
2023-07-05 18:29:22 +0300

    cors fixed

--- backend/app.js
+++ backend/app.js
@@ -34,10 +34,11 @@ mongoose
 app.use(requestLogger);
 app.options('*', cors());
 app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', 'http://razdva.nomoreparties.sbs');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.sendStatus(200);
 });
 app.use(
   cors({
